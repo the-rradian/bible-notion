4c3b83c2771c7c4175af45ab324e8e6fb36135ab
feno <feno_randri@yahoo.ca>
2023-12-29 14:13:54 -0500

    Configured for deployment to Github Pages App is now configured to be built and deployed automatically using Github Actions. Copying verse to clipboard now includes the verse reference.

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ import './style.css'
 const searchBox = document.getElementById('search-box')
 const searchBtn = document.getElementById('search-btn')
 const clearBtn = document.getElementById('clear-btn')
-
+// const toggleBtn = document.getElementById('toggle-btn')
+const select = document.getElementById('version-select')
 const verseText = document.getElementById('verse-text')
+let currentVerse = ""
 
+/** This event listener copies the verse text to the clipboard.
+ * Clipboard API might not work properly on every browser yet
+ */
   verseText.addEventListener('click', () => {
 
-    navigator.clipboard.writeText(verseText.innerHTML);
+    navigator.clipboard.writeText("\"" + verseText.innerHTML.trim() + "\" " + currentVerse); 
     
 })
 
@@ -21,17 +26,18 @@ searchBtn.addEventListener('click', async () => {
 
     try {
       const params = parseReference(reference)
-      const response = await fetch(`http://localhost:8080/verse?book=${params.book}&chapter=${params.chapter}&verse=${params.verse}`)
+      const response = await fetch(`https://notion-bible.uc.r.appspot.com/verse?book=${params.book}&chapter=${params.chapter}&verse=${params.verse}&version=${select.value}`)
       const result = await response.json()
 
       if (result["data"]) {
         verseText.innerHTML = result["data"]["content"]
+        currentVerse = result["data"]["reference"]
       } else
       verseText.innerHTML = "Verse not found"
 
     } catch (error) {
-      console.error("Error: " + error)
-      verseText.innerHTML = "Error: " + error
+      console.error(error)
+      verseText.innerHTML = error
     }
 
 })
@@ -46,7 +52,7 @@ function parseReference(verseString){
       const book = match[1].trim();
       const chapter = parseInt(match[2], 10);
       const verse = parseInt(match[3], 10);
-  
+
       return {
         book: book,
         chapter: chapter,
